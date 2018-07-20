c2a243afe9451ce1424483067f6b5b54473afeb9
Iquwa <ilyess.game@gmail.com>
2018-07-20 14:35:54 +0200

    Update Account.py

--- Account.py
+++ Account.py
@@ -1,8 +1,14 @@
+own=raw_input('Your name: ')
+money=int(input('How much do you want on your account: '))
+
 class Account:
   def __init__(self,owner,balance):
     self.owner = owner
     self.balance = Balance
+    print('Account created')
   def deposit(self,depot):
     self.depot = depot
     self.balance= self.balance + self.depot
     print('You add {}$'.format(self.depot))
+act1=Account(own,money)
+acct1.deposit(money)
