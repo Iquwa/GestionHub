06befb812fb0cfd237f0fd6e28bb0a1a92a37a35
Iquwa <ilyess.game@gmail.com>
2018-07-20 14:33:09 +0200

    Account.py
    An object: Your Account

---
+++ Account.py
@@ -0,0 +1,8 @@
+class Account:
+  def __init__(self,owner,balance):
+    self.owner = owner
+    self.balance = Balance
+  def deposit(self,depot):
+    self.depot = depot
+    self.balance= self.balance + self.depot
+    print('You add {}$'.format(self.depot))
